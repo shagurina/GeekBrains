18226374de2b4cbadf3385dbe6842da26b15af5e
lena <e_chugrova@mail.ru>
2022-06-19 08:55:31 +0300

    Yucca test

--- src/yucca.java
+++ src/yucca.java
@@ -2,70 +2,34 @@ public class yucca {
 
     public static void main(String[] args) {
 
-        hundred();
+        yucca7();
 
     }
 
-
-    public static void hundred() {
-        float[] arrayFloat = new float[100];
-        System.out.print("Числа, которые делятся на 7 с остатком 2: ");
+    public static void yucca7() {
+        float[] arrayFloat = new float[1000];
+        System.out.print("Каждое второе число, которое делится на 7 с остатком 2: ");
         int result = 0;
 
         for (int i = 0; i < arrayFloat.length; i++) {
             arrayFloat[i] = (i + 1) / 7f;
-            // arrayFloat[i] = (int) (((i + 1)/7f % 1)*10);
-            //  System.out.print(arrayFloat[i] + " | ");
-            // if ((int) (((i + 1)/7f % 1)*10) == 2)
-            if ((int) (((i + 1) / 7f % 1) * 10) == 2)
-
+           //if ((int) (((i + 1) / 7f % 1) * 10) == 2)  - вот тут я фильтрую числа, у которых после деления на 7 первая цифра после запятой 2 (внимательно читала задание, ага)
+            if ((int) ((i + 1) / 7f )  == 2)
             { result = result+1;
-
-
-             System.out.print((i + 1) + ":" + arrayFloat[i] + " | ");
+                if  ( result % 2 == 0 ) {
+                    System.out.print((i + 1) + ":" + arrayFloat[i] + " | ");
+                }
             }
-
         }
-
-
-
-        int[] array4 = new int[result];
-
-        for (int j = 0; j < array4.length; j++) {
-            arrayFloat[j] = (j + 1) / 7f;
-            // arrayFloat[i] = (int) (((i + 1)/7f % 1)*10);
-            //  System.out.print(arrayFloat[i] + " | ");
-            // if ((int) (((i + 1)/7f % 1)*10) == 2)
-            if ((int) (((j + 1) / 7f % 1) * 10) == 2)
-
-            {
-                System.out.print((j + 1) + ":" + arrayFloat[j] + " | ");
-            }
-
-        }
-
-
 
         System.out.println();
         System.out.println("---------------------------------------");
         System.out.println("Количество чисел, которые делятся на 7 с остатком 2: " + result);
 
-
-
-
     }
 }
 
 
-          //  System.out.print(arrayFloat[i] + " | ");
-//            for (int j = 0; j < arrayFloat.length; j++) {
-//                int[] arrayInt = new int[];
-//                arrayInt[j] = int(arrayFloat[i]);
-//            }
-//            String [] arrStr = new String[100];
-//            for (int j = 0; j < arrayFloat.length; j++) {
-//                arrStr[j] = Float.toString(arrayFloat[i]);
-//
 
 
 
